7ac40762ff8f26c7869a8b22b9f704a7846f7b6e
立石　純也 <dorasmapnogizaka46@tateishijunyanoMacBook-Air.local>
2020-10-18 20:12:22 +0900

    Study

---
+++ Study01.java
@@ -0,0 +1,66 @@
+public class Study01{
+    public static void main(String[] args){
+      System.out.println(83);
+      System.out.println(82+18);
+      System.out.println("5+2");
+      System.out.println(15/3);
+      System.out.println(3*8);
+      System.out.println(8%3);
+      System.out.println("こんにちは"+"僕ドラえもんです");
+
+      int numbers;
+      numbers=26;
+
+      System.out.println(numbers);
+
+      String name;
+      name="松村沙友理";
+
+      System.out.println(name);
+
+      int number1 = 5;
+      int number2 = 2 ;
+
+      System.out.println(number1*number2);
+
+      String text = "プログラミングをしてみよう";
+
+      System.out.println("Javaで"+text);
+
+      int number3 = 7;
+
+      number3 *= 3 ;
+
+      System.out.println(number3);
+
+      number3 ++;
+
+      System.out.println(number3);
+
+
+      double x = 10.2;
+
+      double y = 2.8;
+
+      System.out.println(x + y);
+
+      System.out.println(x - y);
+
+
+      int month = 3;
+      int date = 25;
+
+      System.out.println("２００５年"+month+"月"+date+"日");
+
+      System.out.println(7/2.0);
+
+
+      int c = 14;
+      int d = 2;
+      System.out.println(c / d);
+    
+    //小数点まで計算するときはkeisannsurutokihadoubleを用いる。
+      System.out.println((double)c / d);
+
+    }
+}
